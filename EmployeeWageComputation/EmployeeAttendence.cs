eec7076ac111e69d5620e3b3d6f9dfd01b55347b
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-11 10:49:07 +0530

    [Prasanna] Add: Implemented Employee Attendence

---
+++ EmployeeWageComputation/EmployeeAttendence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class EmployeeAttendence
+    {
+        public static void Attendence() 
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0,2);
+            if (empcheck == 0)
+                Console.WriteLine("Employee is Present");
+            else Console.WriteLine("Employeee is Absent");
+        }
+    }
+}
