5f770e66f4b54838ab7f767fcfad953ad8effa0d
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-20 18:08:11 +0530

    [Prasanna] Add:Implemented the Total Wages

--- EmployeeWageComputation/EmployeeAttendence.cs
+++ EmployeeWageComputation/EmployeeAttendence.cs
@@ -5,23 +5,36 @@ namespace EmployeeWageComputation
     class EmployeeAttendence
     {
         const int FULL_DAY_HR = 8, PART_DAY_HR=4, IS_FULL_TIME=0, IS_PART_TIME=1;
-        int TOTAL_WORKING_DAYS, TOTAL_WORKING_HRS, WAGE_PER_HR;
-        string CompanyName;
-        public static void Attendence() 
+        int TOTAL_WORKING_DAYS, TOTAL_WORKING_HRS, WAGE_PER_HR, totalEmpWage;
+        string CompanyName = "";
+        Random random = new Random();
+        public EmployeeAttendence()
         {
-            Random random = new Random();
-            int empcheck = random.Next(0,2);
-            if (empcheck == 0)
-                Console.WriteLine("Employee is Present");                                          
-            else Console.WriteLine("Employeee is Absent");                                         
+            Console.WriteLine("Enter company name");
+            this.CompanyName = Console.ReadLine();
+            Console.WriteLine("Wages per hour");
+            this.WAGE_PER_HR = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("NUmber of working days");
+            this.TOTAL_WORKING_DAYS = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Total working hours");
+            this.TOTAL_WORKING_HRS = Convert.ToInt32(Console.ReadLine());
         }
-        public void CalculateEmpWage(string CompanyName, int WAGE_PER_HR, int TOTAL_WORKING_DAYS, int TOTAL_WORKING_HRS )
+        public void Attendence()
         {
-            int totalEmpWage = 0, empHrs = 0;
             Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == 0)
+                Console.WriteLine("Employee is Present");
+            else Console.WriteLine("Employeee is Absent");
+        }
+        public void CalculateEmpWage( )
+        {
+            int empHrs = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
             for (int i = 0; i < TOTAL_WORKING_DAYS && empHrs < TOTAL_WORKING_HRS; i++)
             {
-                int empCheck = random.Next(0, 3);
+                
                 switch (empCheck)
                 {
                     case IS_FULL_TIME:
@@ -36,7 +49,12 @@ namespace EmployeeWageComputation
                 }
             }
             totalEmpWage = WAGE_PER_HR * empHrs;
-            Console.WriteLine(CompanyName +"------>"+totalEmpWage);
+            Console.WriteLine(this.CompanyName +"------>"+totalEmpWage);
+        }
+        public string toString()
+        {
+            return "Daily wages of" + this.CompanyName + " is " + totalEmpWage;
+
         }
 
     }
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,12 +7,15 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
-            Console.WriteLine("Welcome to Employee Wage Computation");
-            EmployeeAttendence.Attendence();
             EmployeeAttendence employeeattendence1 = new EmployeeAttendence();
-            employeeattendence1.CalculateEmpWage( "Amazon",200,20,8);
+            employeeattendence1.Attendence();
+            employeeattendence1.CalculateEmpWage();
+            employeeattendence1.toString();
             EmployeeAttendence employeeattendence2 = new EmployeeAttendence();
-            employeeattendence2.CalculateEmpWage("Flipkart",300,20,8);
+            employeeattendence2.Attendence();   
+            employeeattendence2.CalculateEmpWage();
+            employeeattendence2.toString();
+
         }
     }
 }
