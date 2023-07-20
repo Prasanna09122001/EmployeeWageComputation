cc40e382265db6fc824ae1d5cde4b18e4da5a355
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-20 20:32:07 +0530

    [Prasanna] Add:implement to Wage for Multiple Company

---
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmployeeWage
+    {
+        EmployeeAttendence[] Array = new EmployeeAttendence[10];
+        int companycount = 0;
+        public void CompanyArray(string CompanyName, int WAGE_PER_HR, int TOTAL_WORKING_DAYS, int TOTAL_WORKING_HRS)
+        {
+            Array[companycount] = new EmployeeAttendence(CompanyName, WAGE_PER_HR, TOTAL_WORKING_DAYS, TOTAL_WORKING_HRS);
+            companycount++;
+        }
+        public void CalculateWage()
+        {
+            for (int i = 0; i < companycount; i++)
+            {
+                EmployeeAttendence empwage = Array[i];
+                empwage.CalculateEmpWage();
+                empwage.Companywage();
+            }
+        }
+    }
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,14 +7,12 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
-            EmployeeAttendence employeeattendence1 = new EmployeeAttendence("Apple",200,20,8);
-            employeeattendence1.Attendence();
-            employeeattendence1.CalculateEmpWage();
-            employeeattendence1.Companywage();
-            EmployeeAttendence employeeattendence2 = new EmployeeAttendence("Amazon",150,20,9);
-            employeeattendence2.Attendence();   
-            employeeattendence2.CalculateEmpWage();
-            employeeattendence2.Companywage();
+            EmployeeWage Wage = new EmployeeWage();
+            Wage.CompanyArray("Apple", 200, 20, 8);
+            Wage.CompanyArray("Amazon", 250, 22, 9);
+            Wage.CompanyArray("FlipKart", 230, 20, 9);
+            Wage.CalculateWage();
+           
 
         }
     }
